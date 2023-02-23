bdda609f3c97ad9852dedf3770c58c9e55419aa4
Михаил <misga.yats@gmail.com>
2023-02-23 19:36:37 +0100

    main code structure

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import json
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
+load_dotenv()
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.getenv('API_KEY')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
